da0b3cc944f3a31778e4d57a17f9fed90576b323
Ricardo Olsen <olsen.ricardo@gmail.com>
2026-08-26 18:03:47 -0300

    Migrate ESLint config to flat config format (v9.0.0+)

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import n8nNodesBase from 'eslint-plugin-n8n-nodes-base';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+	{
+		ignores: ['**/*.js', '**/node_modules/**', '**/dist/**'],
+	},
+	{
+		files: ['package.json'],
+		plugins: {
+			'n8n-nodes-base': n8nNodesBase,
+		},
+		rules: {
+			...n8nNodesBase.configs.community.rules,
+			'n8n-nodes-base/community-package-json-name-still-default': 'off',
+		},
+	},
+	{
+		files: ['./credentials/**/*.ts', './nodes/**/*.ts'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				sourceType: 'module',
+				extraFileExtensions: ['.json'],
+				ecmaVersion: 2022,
+			},
+			globals: {
+				browser: true,
+				es6: true,
+				node: true,
+			},
+		},
+		plugins: {
+			'n8n-nodes-base': n8nNodesBase,
+		},
+		rules: {
+			...n8nNodesBase.configs.nodes.rules,
+			'n8n-nodes-base/node-execute-block-missing-continue-on-fail': 'off',
+			'n8n-nodes-base/node-resource-description-filename-against-convention': 'off',
+		},
+	},
+];
